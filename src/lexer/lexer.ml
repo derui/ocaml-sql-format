04be296ba8d88815f5aaa1b4c1bcf5dd02790c25
derui <derutakayu@gmail.com>
2023-06-03 10:19:55 +0900

    feat: add regexp for quoted_id

--- src/lexer/lexer.ml
+++ src/lexer/lexer.ml
@@ -10,6 +10,16 @@ let from_regex =
 
 let space = [%sedlex.regexp? Plus (Chars " \t\r\n")]
 
+let letter = [%sedlex.regexp? 'a' .. 'z' | 'A' .. 'Z' | 0x0153 .. 0xfffd]
+
+let digit = [%sedlex.regexp? '0' .. '9']
+
+let id_part =
+  [%sedlex.regexp? ('@' | '#' | letter), Star (letter | '_' | digit)]
+
+let quoted_id =
+  [%sedlex.regexp? id_part | '"', Star ("\"\"" | Sub (any, '"')), '"']
+
 let rec token buf accum =
   match%sedlex buf with
   | select_regex ->
@@ -18,8 +28,7 @@ let rec token buf accum =
     token buf (Sy_keyword (Ky_from (Sedlexing.Utf8.lexeme buf)) :: accum)
   | '*' -> token buf (Sy_keyword Ky_asterisk :: accum)
   | eof -> List.rev accum
-  | id_start, Star id_continue ->
-    token buf (Sy_ident (Sedlexing.Utf8.lexeme buf) :: accum)
+  | quoted_id -> token buf (Sy_ident (Sedlexing.Utf8.lexeme buf) :: accum)
   | space -> token buf accum
   | any -> token buf accum
   | _ -> failwith "Malformed source"
