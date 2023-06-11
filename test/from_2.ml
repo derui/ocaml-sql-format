d9142e4ebfd5ca4073243ac272fb277193e1cca3
derui <derutakayu@gmail.com>
2023-06-11 09:47:32 +0900

    test: add case for from

---
+++ test/from_2.ml
@@ -0,0 +1,19 @@
+module F = Formatter
+module P = Parser.Parser
+
+let actual =
+  {|
+  SELECT * from test_table abc, next as Next,
+               third
+|}
+
+let option = F.Options.default
+
+let%test_unit "multiple tables in from for AST" =
+  let actual_ast = Util.parse actual
+  and expect_ast = Util.parse @@ F.from_string actual ~option in
+  assert (List.for_all2 Parser.Ast.equal_entry actual_ast expect_ast)
+
+let%expect_test "multiple tables in from for formatting" =
+  print_endline @@ F.from_string actual ~option;
+  [%expect {| SELECT *  FROM test_table AS abc,next AS Next,third |}]
