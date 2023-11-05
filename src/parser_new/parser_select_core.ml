8142c778254f1019e670fbb27eb8eae48c31fa82
derui <derutakayu@gmail.com>
2023-11-05 09:48:47 +0900

    feat: add select_core

--- src/parser_new/parser_select_core.ml
+++ src/parser_new/parser_select_core.ml
@@ -4,34 +4,74 @@ include (
     open M.Syntax
     open M.Let_syntax
     module T = Types.Token
+    module K = Parser_monad.Kind
     module Kw = Types.Keyword
 
-    let rec parse expr () = select expr
+    module type Data = sig
+      val from_clause : Type.parser
 
-    and result_column expr () =
-      let expr' = M.skip >>= expr in
-      let star = M.bump_when T.Op_star in
-      let table_name =
-        M.bump_match (function
-          | T.Tok_ident _ -> true
-          | _ -> false)
-        *> M.bump_when T.Tok_period *> M.bump_when T.Op_star
-      in
-      let* () = expr' <|> star <|> table_name in
-      M.return ()
+      val where_clause : Type.parser
 
-    and select expr =
-      let* () = M.bump_kw Kw.Kw_select in
-      let* () = M.bump_kw Kw.Kw_distinct <|> M.bump_kw Kw.Kw_all <|> M.skip in
-      let result_columns =
-        let* () = M.skip >>= result_column expr in
-        M.many (M.bump_when T.Tok_comma >>= result_column expr) *> M.skip
-      in
-      let* () = result_columns in
-      M.skip
+      val group_by_clause : Type.parser
+
+      val window_clause : Type.parser
+
+      val having_clause : Type.parser
+
+      val result_column : Type.parser
+
+      val expr : Type.parser
+    end
+
+    module P (D : Data) = struct
+      let select () =
+        let* () = M.bump_kw Kw.Kw_select in
+        let* () = M.bump_kw Kw.Kw_distinct <|> M.bump_kw Kw.Kw_all <|> M.skip in
+        let result_columns =
+          let* _ =
+            Subparser.nonempty_list ~sep:(M.bump_when T.Tok_comma)
+              (M.skip >>= D.result_column)
+          in
+          M.return ()
+        in
+        let* () = result_columns in
+        let* () = D.from_clause () <|> M.skip in
+        let* () = D.where_clause () <|> M.skip in
+        let* () = D.group_by_clause () <|> M.skip in
+        let* () = D.having_clause () <|> M.skip in
+        let* () = D.window_clause () <|> M.skip in
+        M.return ()
+
+      let values () =
+        let* () = M.bump_kw Kw.Kw_values in
+        let sep = M.bump_when T.Tok_comma in
+        let values =
+          Wrapping.parens (Subparser.nonempty_list ~sep (M.skip >>= D.expr))
+        in
+        let* _ = Subparser.nonempty_list ~sep values in
+        M.return ()
+
+      let parse () =
+        let p = select () <|> values () in
+        M.start_syntax K.N_select_core p
+    end
 
     let generate taker () =
-      let expr = taker Parser_monad.Kind.N_expr in
-      parse expr ()
+      let module P = P (struct
+        let from_clause = taker Parser_monad.Kind.N_from_clause
+
+        let group_by_clause = taker Parser_monad.Kind.N_group_by_clause
+
+        let having_clause = taker Parser_monad.Kind.N_having_clause
+
+        let where_clause = taker Parser_monad.Kind.N_where_clause
+
+        let window_clause = taker Parser_monad.Kind.N_window_clause
+
+        let result_column = taker Parser_monad.Kind.N_result_column
+
+        let expr = taker Parser_monad.Kind.N_expr
+      end) in
+      P.parse ()
   end :
     Intf.GEN)
---
+++ src/parser_new/test/select_core_test.ml
@@ -0,0 +1,55 @@
+module P = Parser_new
+module M = Parser_monad.Monad
+module S = Sql_syntax
+module PE = Parser_monad.Parse_error
+
+let p = P.Parser_select_core.generate (P.Slot.get_taker ())
+
+let%expect_test "parse " =
+  Util.run {| select 1 |} p |> print_endline;
+  Util.run {| select distinct a,b,c
+                from z
+            |} p
+  |> print_endline;
+  Util.run
+    {| select all a,b,c
+                from z
+                group by b
+            |}
+    p
+  |> print_endline;
+  Util.run
+    {| select all a,b,c
+                from z
+                having b > 0
+            |}
+    p
+  |> print_endline;
+  Util.run
+    {| select all a,b,c
+                from z
+                having b > 0
+                window wname as (partition by a)
+            |}
+    p
+  |> print_endline;
+  Util.run {| values (1,3,5 * 3) |} p |> print_endline;
+  Util.run {| values (1,3,5 * 3), (3,7,9) |} p |> print_endline;
+
+  [%expect
+    {|
+    select 1
+    select distinct a,b,c
+    from z
+    select all a,b,c
+    from z
+    group by b
+    select all a,b,c
+    from z
+    having b > 0
+    select all a,b,c
+    from z
+    having b > 0
+    window wname as (partition by a)
+    values (1,3,5 * 3)
+    values (1,3,5 * 3), (3,7,9) |}]
