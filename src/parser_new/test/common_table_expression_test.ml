4f7040a02537ea84552e0201f4ebbe40b3c56029
derui <derutakayu@gmail.com>
2023-11-07 07:47:33 +0900

    test: add case

--- src/parser_new/test/common_table_expression_test.ml
+++ src/parser_new/test/common_table_expression_test.ml
@@ -11,10 +11,12 @@ let%expect_test "parse " =
   Util.run {| some_table as not (select 1) |} p |> print_endline;
   Util.run {| some_table as not materialized (select a,b,c from table_c) |} p
   |> print_endline;
+  Util.run {| some_table (c1,c2,c3) as (select 1) |} p |> print_endline;
 
   [%expect
     {|
     some_table as (select 1)
     some_table as materialized (select 1)
     some_table as not (select 1)
-    some_table as not materialized (select a,b,c from table_c) |}]
+    some_table as not materialized (select a,b,c from table_c)
+    some_table (c1,c2,c3) as (select 1) |}]
