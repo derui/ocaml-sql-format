e094a80267b76c14a4198007c3110c8abe2b144a
derui <derutakayu@gmail.com>
2023-11-23 09:52:12 +0900

    fix: add some rewriter

--- src/rewriter/rewriters.ml
+++ src/rewriter/rewriters.ml
@@ -2,11 +2,14 @@ module Sp = Support
 module K = Sql_syntax.Kind
 module R = Sql_syntax.Raw
 
-let rewrite_expr env raw =
+let rec rewrite_expr env raw =
   let expr_rewriter env raw =
     Sp.choice
       [ (`leaf K.L_ident, fun env r -> Sp.space ~leading:1 env r)
       ; (`leaf K.L_qmark, fun env r -> Sp.space ~leading:1 env r)
+      ; (`leaf K.L_string, fun env r -> Sp.space ~leading:1 env r)
+      ; (`leaf K.L_numeric, fun env r -> Sp.space ~leading:1 env r)
+      ; (`leaf K.L_blob, fun env r -> Sp.space ~leading:1 env r)
       ]
       env raw
   in
