312708112bbe0e3f0cf7f973d08cdfe4b0562b40
derui <derutakayu@gmail.com>
2023-12-04 08:14:12 +0900

    feat: add some CST for select_stmt

--- src/sql_syntax/cst.ml
+++ src/sql_syntax/cst.ml
@@ -15,6 +15,7 @@ module Sql_stmt_list = struct
     | _ -> None
 end
 
+(** CST for sql_stmt *)
 module Sql_stmt = struct
   module T = Types.Token
 
@@ -55,22 +56,57 @@ module Sql_stmt = struct
     | _ -> None
 end
 
+(** CST for begin_stmt *)
 module Begin_stmt = struct
-  module T = Types.Token
+  module K = Types.Keyword
 
   (** [kw_begin raw] selects keyword [begin] *)
-  let kw_begin = Cst_support.is_keyword Types.Keyword.Kw_begin
+  let kw_begin = Cst_support.is_keyword K.Kw_begin
 
   (** [kw_transaction raw] selects keyword [transaction] *)
-  let kw_transaction = Cst_support.is_keyword Types.Keyword.Kw_transaction
+  let kw_transaction = Cst_support.is_keyword K.Kw_transaction
 end
 
+(** CST for rollback_stmt *)
 module Rollback_stmt = struct
-  module T = Types.Token
+  module K = Types.Keyword
 
   (** [kw_rollback raw] selects keyword [rollback] *)
-  let kw_rollback = Cst_support.is_keyword Types.Keyword.Kw_rollback
+  let kw_rollback = Cst_support.is_keyword K.Kw_rollback
 
   (** [kw_transaction raw] selects keyword [transaction] *)
-  let kw_transaction = Cst_support.is_keyword Types.Keyword.Kw_transaction
+  let kw_transaction = Cst_support.is_keyword K.Kw_transaction
+end
+
+(** CST for select_stmt *)
+module Select_stmt = struct
+  module K = Types.Keyword
+
+  (** [kw_select raw] selects keyword [select] *)
+  let kw_select = Cst_support.is_keyword K.Kw_select
+
+  (** [kw_distinct raw] selects keyword [distinct] *)
+  let kw_distinct = Cst_support.is_keyword K.Kw_distinct
+
+  (** [kw_union raw] selects keyword [union] *)
+  let kw_union = Cst_support.is_keyword K.Kw_union
+
+  (** [kw_all raw] selects keyword [all] *)
+  let kw_all = Cst_support.is_keyword K.Kw_all
+
+  (** [kw_intersect raw] selects keyword [intersect] *)
+  let kw_intersect = Cst_support.is_keyword K.Kw_intersect
+
+  (** [kw_except raw] selects keyword [except] *)
+  let kw_except = Cst_support.is_keyword K.Kw_except
+
+  (** [n_with_clause raw] selects [with_clause] *)
+  let n_with_clause = function
+    | Raw.Node { kind = Kind.N_with_clause; _ } as v -> Some v
+    | _ -> None
+
+  (** [n_select_core raw] selects [select_core] *)
+  let n_select_core = function
+    | Raw.Node { kind = Kind.N_select_core; _ } as v -> Some v
+    | _ -> None
 end
